0e6319c61c008a818e7c283cf669f7f79cfd1707
Rob Fletcher <robert.w.fletcher@gmail.com>
2010-06-25 06:43:41 +0100

    vote works

--- grails-app/controllers/votething/poll/PollController.groovy
+++ grails-app/controllers/votething/poll/PollController.groovy
@@ -2,7 +2,7 @@ package votething.poll
 
 import grails.plugins.springsecurity.Secured
 import votething.auth.Role
-import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND
+import static javax.servlet.http.HttpServletResponse.*
 import votething.auth.User
 
 class PollController {
@@ -11,27 +11,37 @@ class PollController {
 
 	def springSecurityService
 
-	@Secured(Role.USER)
+	@Secured("ROLE_USER")
 	def show = {
 		def pollInstance = params.uri ? Poll.findByUri(params.uri) : null
-		if (!pollInstance) {
-			response.sendError SC_NOT_FOUND
-		} else {
+		if (pollInstance) {
 			[pollInstance: pollInstance]
+		} else {
+			response.sendError SC_NOT_FOUND
 		}
 	}
 
-	@Secured(Role.USER)
+	@Secured("ROLE_USER")
 	vote = {
 		def pollInstance = params.id ? Poll.read(params.id) : null
-		if (!pollInstance) {
+		if (pollInstance) {
+			def voteInstance = new Vote(user: loggedInUser, poll: pollInstance, option: params.int("option"))
+			log.info "vote: saving ${voteInstance.dump()}"
+			if (voteInstance.save()) {
+				redirect action: "show", params: [uri: pollInstance.uri]
+			} else {
+				log.error "vote: failed to save $voteInstance.errors"
+				response.sendError SC_INTERNAL_SERVER_ERROR
+			}
+		} else {
 			log.error "vote: no poll with id $params.id exists"
 			response.sendError SC_NOT_FOUND
-		} else {
-			def userDetails = springSecurityService.principal
-			def user = User.get(userDetails.id)
-			new Vote(user: user, poll: poll, option: params.int("option"))
 		}
+	}
+
+	private User getLoggedInUser() {
+		def userDetails = springSecurityService.principal
+		userDetails ? User.get(userDetails.id) : null
 	}
 
 }
--- test/unit/votething/poll/PollControllerSpec.groovy
+++ test/unit/votething/poll/PollControllerSpec.groovy
@@ -4,23 +4,31 @@ import grails.plugin.spock.ControllerSpec
 import spock.lang.Shared
 import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND
 import static javax.servlet.http.HttpServletResponse.SC_OK
+import votething.auth.User
+import org.apache.commons.lang.math.RandomUtils
+import grails.plugins.springsecurity.SpringSecurityService
 
 class PollControllerSpec extends ControllerSpec {
 
 	@Shared List<Poll> polls = (1..3).collect { i ->
-		new Poll(title: "Poll $i", uri: "poll$i")
+		new Poll(title: "Poll $i", uri: "poll$i", options: ["Option 1", "Option 2", "Option 3"])
 	}
 
+	User user = new User(username: "blackbeard")
+
 	def setup() {
+		mockLogging PollController, true
 		mockDomain Poll, polls
+		mockDomain Vote
+		mockDomain User, [user]
 	}
 
 	def "The show action requires a valid URI parameter"() {
-		when: "the show action is invoked without a URI"
+		when: "the show action is invoked without a valid URI"
 		controller.params.uri = uri
 		controller.show()
 
-		then: "the response status is not-found"
+		then: "the response status is 404"
 		controller.response.status == SC_NOT_FOUND
 
 		where:
@@ -29,17 +37,53 @@ class PollControllerSpec extends ControllerSpec {
 
 	def "The show action finds the poll with the specified URI"() {
 		when: "the show action is invoked with a valid URI"
-		controller.params.uri = uri
+		controller.params.uri = poll.uri
 		def model = controller.show()
 
-		then: "the response status is OK"
+		then: "the response status is 200"
 		controller.response.status == SC_OK
 
 		and: "the correct poll is added to the model"
 		model.pollInstance == poll
 
 		where:
-		uri << polls.uri
 		poll << polls
+	}
+
+	def "The vote action requires a valid id"() {
+		when: "the vote action is invoked without a valid id"
+		controller.params.id = id
+		controller.vote()
+
+		then: "the respons status is 404"
+		controller.response.status == SC_NOT_FOUND
+
+		where:
+		id << [null, 9]
+	}
+
+	def "A user can vote on a poll"() {
+		given: "a logged in user"
+		controller.springSecurityService = Mock(SpringSecurityService)
+		controller.springSecurityService.principal >> [id: user.id]
+
+		when: "the user submits a vote"
+		controller.params.id = poll.id
+		controller.params.option = option
+		controller.vote()
+
+		then: "the user is redirected to the show action"
+		controller.redirectArgs.action == "show"
+		controller.redirectArgs.params == [uri: poll.uri]
+
+		and: "the vote is registered"
+		Vote.count() == 1
+		def vote = Vote.findByUserAndPoll(user, poll)
+		vote != null
+		vote.option == option
+
+		where:
+		poll << polls
+		option << [0, 1, 2]
 	}
 }
